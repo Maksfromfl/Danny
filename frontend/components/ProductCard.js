feeb992b1968a1017ee56418df81e9e1ea4aa972
Maksfromfl <max1442@me.com>
2026-05-15 17:57:30 +0300

    Add files via upload

---
+++ frontend/components/ProductCard.js
@@ -0,0 +1,11 @@
+
+export default function ProductCard({ product }) {
+  return (
+    <div className="card">
+      <img src={product.image} alt={product.name} />
+      <h3>{product.name}</h3>
+      <p>{product.price} €</p>
+      <button>Добавить в корзину</button>
+    </div>
+  )
+}
